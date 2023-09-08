58206c82a504b6bc4c227f55efa09334193da60e
Uli Köhler <uli@techoverflow.net>
2023-09-09 00:05:16 +0200

    Fix "wg pubkey" crashing on Windows

--- guardmywire.py
+++ guardmywire.py
@@ -18,7 +18,7 @@ def generate_wireguard_keys():
     Returns (private_key, public_key), both strings
     """
     privkey = subprocess.check_output("wg genkey", shell=True).decode("utf-8").strip()
-    pubkey = subprocess.check_output(f"echo '{privkey}' | wg pubkey", shell=True).decode("utf-8").strip()
+    pubkey = subprocess.check_output(f"echo -n '{privkey}' | wg pubkey", shell=True).decode("utf-8").strip()
     return (privkey, pubkey)
 
 def generate_wireguard_psk():
